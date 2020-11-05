d06ff321ad371f0d3f6af544ff34d8d7f59ab81c
AxQus <tsklyt@wp.pl>
2020-11-05 19:14:50 +0100

    add bg

--- strony/1bs/js/main.js
+++ strony/1bs/js/main.js
@@ -9,7 +9,15 @@ jQuery(document).ready(function() {
 	                   , "1bs/img/bg/2.jpg"
 	                   , "1bs/img/bg/3.jpg"
 					   , "1bs/img/bg/4.jpg"
-	                   , "1bs/img/bg/5.jpg"
+					   , "1bs/img/bg/5.jpg"
+					   , "1bs/img/bg/6.jpg"
+	                   , "1bs/img/bg/7.jpg"
+					   , "1bs/img/bg/8.jpg"
+					   , "1bs/img/bg/9.jpg"
+					   , "1bs/img/bg/10.jpg"
+	                   , "1bs/img/bg/11.jpg"
+					   , "1bs/img/bg/12.jpg"
+	                   , "1bs/img/bg/13.jpg"
 	                  ], {duration: 3000, fade: 750});
 					 
 });
